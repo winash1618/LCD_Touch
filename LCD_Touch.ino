241e2d032306855b73a82dcdfd211f965ae4be98
winash1.618 <asharafbarcalona786@gmail.com>
2024-03-07 20:32:27 +0400

    final

--- LCD_Touch.ino
+++ LCD_Touch.ino
@@ -8,6 +8,22 @@
 // create enum of button names
 // Buttons for the main button
 
+enum STYLE {
+  COL0 = 10,
+  COL1 = 110,
+  COL2 = 180,
+  COL3 = 250,
+  COL4 = 330,
+  COL5 = 400,
+  COL6 = 480,
+  ROW0 = 10,
+  ROW1 = 60,
+  ROW2 = 110,
+  ROW3 = 160,
+  ROW4 = 210,
+  ROW5 = 260
+};
+
 enum BUTTON_CLASS {
   MainButton = 1,
   MemoryButton = 2,
@@ -21,11 +37,11 @@ enum MAIN_BUTTON_NAME {
   Stop = 2,
   Live = 3,
   Manual = 4,
-  Go = 5
+  Go = 5,
   Status = 6
 };
 
-enum MemoryButtonName {
+enum MEMORY_BUTTON_NAME {
   Memory1 = 1,
   Memory2 = 2,
   Memory3 = 3,
@@ -33,7 +49,7 @@ enum MemoryButtonName {
   Memory5 = 5
 };
 
-enum ValueButtonName {
+enum VALUE_BUTTON_NAME {
   Value1 = 1,
   Value2 = 2,
   Value3 = 3,
@@ -41,12 +57,13 @@ enum ValueButtonName {
   Value5 = 5
 };
 
-enum SpeedButtonName {
+enum SPEED_BUTTON_NAME {
   sp1 = 1,
   sp2 = 2,
   sp3 = 3,
   sp4 = 4,
   sp5 = 5,
+  sp6 = 6
 };
 
 enum DisplayButtonName {
@@ -60,80 +77,42 @@ enum DisplayButtonName {
   Minus3 = 8
 };
 
-// create array of buttons
+
 Button btn[] = {
-  Button(10, 10, 100, 50, "Home", BUTTON_CLASS::MainButton, MAIN_BUTTON_NAME::Home),
-  Button(10, 60, 100, 100, "Stop", BUTTON_CLASS::MainButton, MAIN_BUTTON_NAME::Stop),
-  Button(10, 110, 100, 150, "Live", BUTTON_CLASS::MainButton, MAIN_BUTTON_NAME::Live),
-  Button(10, 160, 100, 200, "Manual", BUTTON_CLASS::MainButton, MAIN_BUTTON_NAME::Manual),
-  Button(10, 210, 100, 250, "Go", BUTTON_CLASS::MainButton, MAIN_BUTTON_NAME::Go),
-  Button(10, 260, 80, 310, "sp1", BUTTON_CLASS::SpeedButton, SpeedButtonName::sp1),
-  Button(90, 260, 160, 310, "sp2", BUTTON_CLASS::SpeedButton, SpeedButtonName::sp2),
-  Button(170, 260, 240, 310, "sp3", BUTTON_CLASS::SpeedButton, SpeedButtonName::sp3),
-  Button(250, 260, 320, 310, "sp4", BUTTON_CLASS::SpeedButton, SpeedButtonName::sp4),
-  Button(320, 260, 390, 310, "sp5", BUTTON_CLASS::SpeedButton, SpeedButtonName::sp5),
-  Button(400, 260, 470, 310, "sp6", BUTTON_CLASS::SpeedButton, SpeedButtonName::sp6),
-  Button(110, 10, 150, 50, "+", BUTTON_CLASS::DisplayButton, DisplayButtonName::Plus1),
-  Button(160, 10, 200, 50, "+", BUTTON_CLASS::DisplayButton, DisplayButtonName::Plus2),
-  Button(210, 10, 250, 50, "+", BUTTON_CLASS::DisplayButton, DisplayButtonName::Plus3),
-  Button(110, 60, 250, 100, "000", BUTTON_CLASS::DisplayButton, DisplayButtonName::Digit1),
-  Button(110, 110, 150, 150, "-", BUTTON_CLASS::DisplayButton, DisplayButtonName::Minus1),
-  Button(160, 110, 200, 150, "-", BUTTON_CLASS::DisplayButton, DisplayButtonName::Minus2),
-  Button(210, 110, 250, 150, "-", BUTTON_CLASS::DisplayButton, DisplayButtonName::Minus3),
-  Button(110, 160, 250, 200, "Status", BUTTON_CLASS::DisplayButton, DisplayButtonName::Status),
-  Button(110, 210, 250, 250, "Status 2", BUTTON_CLASS::DisplayButton, DisplayButtonName::Status2),
-  Button(260, 10, 360, 50, "Memory 1", BUTTON_CLASS::MemoryButton, MemoryButtonName::Memory1),
-  Button(370, 10, 470, 50, "Value 1", BUTTON_CLASS::ValueButton, ValueButtonName::Value1),
-  Button(260, 60, 360, 100, "Memory 2", BUTTON_CLASS::MemoryButton, MemoryButtonName::Memory2),
-  Button(370, 60, 470, 100, "Value 2", BUTTON_CLASS::ValueButton, ValueButtonName::Value2),
-  Button(260, 110, 360, 150, "Memory 3", BUTTON_CLASS::MemoryButton, MemoryButtonName::Memory3),
-  Button(370, 110, 470, 150, "Value 3", BUTTON_CLASS::ValueButton, ValueButtonName::Value3),
-  Button(260, 160, 360, 200, "Memory 4", BUTTON_CLASS::MemoryButton, MemoryButtonName::Memory4),
-  Button(370, 160, 470, 200, "Value 4", BUTTON_CLASS::ValueButton, ValueButtonName::Value4),
-  Button(260, 210, 360, 250, "Memory 5", BUTTON_CLASS::MemoryButton, MemoryButtonName::Memory5),
-  Button(370, 210, 470, 250, "Value 5", BUTTON_CLASS::ValueButton, ValueButtonName::Value5)
+  Button(STYLE::COL0, STYLE::ROW0, STYLE::COL1, STYLE::ROW1, "Home", BUTTON_CLASS::MainButton, MAIN_BUTTON_NAME::Home),
+  Button(STYLE::COL0, STYLE::ROW1, STYLE::COL1, STYLE::ROW2, "Stop", BUTTON_CLASS::MainButton, MAIN_BUTTON_NAME::Stop),
+  Button(STYLE::COL0, STYLE::ROW2, STYLE::COL1, STYLE::ROW3, "Live", BUTTON_CLASS::MainButton, MAIN_BUTTON_NAME::Live),
+  Button(STYLE::COL0, STYLE::ROW3, STYLE::COL1, STYLE::ROW4, "Manual", BUTTON_CLASS::MainButton, MAIN_BUTTON_NAME::Manual),
+  Button(STYLE::COL0, STYLE::ROW4, STYLE::COL1, STYLE::ROW5, "Go", BUTTON_CLASS::MainButton, MAIN_BUTTON_NAME::Go),
+  
+  Button(STYLE::COL0, STYLE::ROW5, STYLE::COL1, STYLE::ROW5, "sp1", BUTTON_CLASS::SpeedButton, SPEED_BUTTON_NAME::sp1),
+  Button(STYLE::COL1, STYLE::ROW5, STYLE::COL2, STYLE::ROW5, "sp2", BUTTON_CLASS::SpeedButton, SPEED_BUTTON_NAME::sp2),
+  Button(STYLE::COL2, STYLE::ROW5, STYLE::COL3, STYLE::ROW5, "sp3", BUTTON_CLASS::SpeedButton, SPEED_BUTTON_NAME::sp3),
+  Button(STYLE::COL3, STYLE::ROW5, STYLE::COL4, STYLE::ROW5, "sp4", BUTTON_CLASS::SpeedButton, SPEED_BUTTON_NAME::sp4),
+  // Button(STYLE::COL4, STYLE::ROW5, STYLE::COL5, STYLE::ROW5, "sp5", BUTTON_CLASS::SpeedButton, SPEED_BUTTON_NAME::sp5),
+  // Button(STYLE::COL5, STYLE::ROW5, STYLE::COL6, STYLE::ROW5, "sp6", BUTTON_CLASS::SpeedButton, SPEED_BUTTON_NAME::sp6),
+
+  Button(STYLE::COL1, STYLE::ROW0, STYLE::COL2, STYLE::ROW1, "+", BUTTON_CLASS::DisplayButton, DisplayButtonName::Plus1),
+  Button(STYLE::COL2, STYLE::ROW0, STYLE::COL3, STYLE::ROW1, "+", BUTTON_CLASS::DisplayButton, DisplayButtonName::Plus2),
+  Button(STYLE::COL3, STYLE::ROW0, STYLE::COL4, STYLE::ROW1, "+", BUTTON_CLASS::DisplayButton, DisplayButtonName::Plus3),
+  Button(STYLE::COL1, STYLE::ROW1, STYLE::COL4, STYLE::ROW2, "000", BUTTON_CLASS::DisplayButton, DisplayButtonName::Digit1),
+  Button(STYLE::COL1, STYLE::ROW2, STYLE::COL2, STYLE::ROW3, "-", BUTTON_CLASS::DisplayButton, DisplayButtonName::Minus1),
+  Button(STYLE::COL2, STYLE::ROW2, STYLE::COL3, STYLE::ROW3, "-", BUTTON_CLASS::DisplayButton, DisplayButtonName::Minus2),
+  Button(STYLE::COL3, STYLE::ROW2, STYLE::COL4, STYLE::ROW3, "-", BUTTON_CLASS::DisplayButton, DisplayButtonName::Minus3),
+  Button(STYLE::COL1, STYLE::ROW3, STYLE::COL4, STYLE::ROW4, "Status", BUTTON_CLASS::MainButton, MAIN_BUTTON_NAME::Status),
+
+  Button(STYLE::COL4, STYLE::ROW0, STYLE::COL5, STYLE::ROW1, "Memory 1", BUTTON_CLASS::MemoryButton, MEMORY_BUTTON_NAME::Memory1),
+  Button(STYLE::COL5, STYLE::ROW0, STYLE::COL6, STYLE::ROW1, "Value 1", BUTTON_CLASS::ValueButton, VALUE_BUTTON_NAME::Value1),
+  Button(STYLE::COL4, STYLE::ROW1, STYLE::COL5, STYLE::ROW2, "Memory 2", BUTTON_CLASS::MemoryButton, MEMORY_BUTTON_NAME::Memory2),
+  Button(STYLE::COL5, STYLE::ROW1, STYLE::COL6, STYLE::ROW2, "Value 2", BUTTON_CLASS::ValueButton, VALUE_BUTTON_NAME::Value2),
+  Button(STYLE::COL4, STYLE::ROW2, STYLE::COL5, STYLE::ROW3, "Memory 3", BUTTON_CLASS::MemoryButton, MEMORY_BUTTON_NAME::Memory3),
+  Button(STYLE::COL5, STYLE::ROW2, STYLE::COL6, STYLE::ROW3, "Value 3", BUTTON_CLASS::ValueButton, VALUE_BUTTON_NAME::Value3),
+  Button(STYLE::COL4, STYLE::ROW3, STYLE::COL5, STYLE::ROW4, "Memory 4", BUTTON_CLASS::MemoryButton, MEMORY_BUTTON_NAME::Memory4),
+  Button(STYLE::COL5, STYLE::ROW3, STYLE::COL6, STYLE::ROW4, "Value 4", BUTTON_CLASS::ValueButton, VALUE_BUTTON_NAME::Value4),
+  Button(STYLE::COL4, STYLE::ROW4, STYLE::COL5, STYLE::ROW5, "Memory 5", BUTTON_CLASS::MemoryButton, MEMORY_BUTTON_NAME::Memory5),
+  Button(STYLE::COL5, STYLE::ROW4, STYLE::COL6, STYLE::ROW5, "Value 5", BUTTON_CLASS::ValueButton, VALUE_BUTTON_NAME::Value5)
 };
 
-  
-
-// Create Home, Stop, Live, Manual, Go buttons
-// Button homeButton(10, 10, 100, 50, "Home", 1);
-// Button stopButton(10, 60, 100, 100, "Stop", 2);
-// Button liveButton(10, 110, 100, 150, "Live", 3);
-// Button manualButton(10, 160, 100, 200, "Manual", 4);
-// Button goButton(10, 210, 100, 250, "Go", 5);
-
-// Button sp1Button(10, 260, 80, 310, "sp1", 17);
-// Button sp2Button(90, 260, 160, 310, "sp2", 18);
-// Button sp3Button(170, 260, 240, 310, "sp3", 19);
-// Button sp4Button(250, 260, 320, 310, "sp4", 20);
-// Button sp5Button(320, 260, 390, 310, "sp5", 21);
-// Button sp6Button(400, 260, 470, 310, "sp6", 22);
-
-// Button plusButton(110, 10, 150, 50, "+", 6);
-// Button plusButton2(160, 10, 200, 50, "+", 7);
-// Button plusButton3(210, 10, 250, 50, "+", 8);
-
-// Button digitButton(110, 60, 250, 100, "000", 9);
-
-// Button minusButton(110, 110, 150, 150, "-", 10);
-// Button minusButton2(160, 110, 200, 150, "-", 11);
-// Button minusButton3(210, 110, 250, 150, "-", 12);
-
-// Button statusButton(110, 160, 250, 200, "Status", 13);
-// Button statusButton2(110, 210, 250, 250, "Status 2", 24);
-
-
-// Button memoryButton(260, 10, 360, 50, "Memory 1", 13);
-// Button valueButton(370, 10, 470, 50, "Value 1", 13);
-// Button memoryButton2(260, 60, 360, 100, "Memory 2", 14);
-// Button valueButton2(370, 60, 470, 100, "Value 2", 14);
-// Button memoryButton3(260, 110, 360, 150, "Memory 3", 15);
-// Button valueButton3(370, 110, 470, 150, "Value 3", 15);
-// Button memoryButton4(260, 160, 360, 200, "Memory 4", 16);
-// Button valueButton4(370, 160, 470, 200, "Value 4", 16);
-// Button memoryButton5(260, 210, 360, 250, "Memory 5", 23);
-// Button valueButton5(370, 210, 470, 250, "Value 5", 25);
 
 // create value button below memory buttons 
 
@@ -153,227 +132,106 @@ void setup()
   // TP_Dialog();
 
   Serial.println("Drawing...");
-
-  // draw each button in the array
+  GUI_Clear(WHITE);
   for (int i = 0; i < 26; i++) {
     btn[i].draw();
+    if (btn[i].class_id == BUTTON_CLASS::ValueButton) {
+      btn[i].clear();
+      btn[i].displayNumber(EEPROM.read((btn[i].id - 1) * 2));
+    }
   }
-
-  // homeButton.draw();
-  // stopButton.draw();
-  // liveButton.draw();
-  // manualButton.draw();
-  // goButton.draw();
-
-  // sp1Button.draw();
-  // sp2Button.draw();
-  // sp3Button.draw();
-  // sp4Button.draw();
-  // sp5Button.draw();
-  // sp6Button.draw();
-
-  // plusButton.draw();
-  // minusButton.draw();
-
-  // plusButton2.draw();
-  // minusButton2.draw();
-
-  // plusButton3.draw();
-  // minusButton3.draw();
-
-  // digitButton.draw();
-
-  // statusButton.draw();
-  // statusButton2.draw();
-
-  // memoryButton.draw();
-  // valueButton.draw();
-  // valueButton.clear();
-  // valueButton.displayNumber(EEPROM.read(0));
-  // memoryButton2.draw();
-  // valueButton2.draw();
-  // valueButton2.clear();
-  // valueButton2.displayNumber(EEPROM.read(4));
-  // memoryButton3.draw();
-  // valueButton3.draw();
-  // valueButton3.clear();
-  // valueButton3.displayNumber(EEPROM.read(8)));
-  // memoryButton4.draw();
-  // valueButton4.draw();
-  // valueButton4.clear();
-  // valueButton4.displayNumber(EEPROM.read(12));
-  // memoryButton5.draw();
-  // valueButton5.draw();
-  // valueButton5.clear();
-  // valueButton5.displayNumber(EEPROM.read(16));
-  
 }
 TP_DRAW event;
-int i = 0;
+int num = 0;
+void handleDigit(int i);
+void modifyDigit(int i);
 void loop()
 {
   // TP_DrawBoard();
   event = touchEvent();
   if (event.Xpoint != -1 && event.Ypoint != -1) {
-    if (homeButton.contains(event.Xpoint, event.Ypoint)) {
-      Serial.println("Home");
-    } 
-    if (stopButton.contains(event.Xpoint, event.Ypoint)) {
-      Serial.println("Stop");
-    } 
-    if (liveButton.contains(event.Xpoint, event.Ypoint)) {
-      Serial.println("Live");
-    } 
-    if (manualButton.contains(event.Xpoint, event.Ypoint)) {
-      Serial.println("Manual");
-    } 
-    if (goButton.contains(event.Xpoint, event.Ypoint)) {
-      Serial.println("Go");
-    } 
-    if (sp1Button.contains(event.Xpoint, event.Ypoint)) {
-      Serial.println("sp1");
-    } 
-    if (sp2Button.contains(event.Xpoint, event.Ypoint)) {
-      Serial.println("sp2");
-    } 
-    if (sp3Button.contains(event.Xpoint, event.Ypoint)) {
-      Serial.println("sp3");
-    } 
-    if (sp4Button.contains(event.Xpoint, event.Ypoint)) {
-      Serial.println("sp4");
-    } 
-    if (sp5Button.contains(event.Xpoint, event.Ypoint)) {
-      Serial.println("sp5");
-    } 
-    if (sp6Button.contains(event.Xpoint, event.Ypoint)) {
-      Serial.println("sp6");
-    } 
-    if (plusButton.contains(event.Xpoint, event.Ypoint)) {
-      Serial.println("plusButton");
-      i = i + 100;
-      if (i > 999)
-        i = i - 100;
-      digitButton.clear();
-      digitButton.displayNumber(i);
-      // GUI_DisNum(120, 70, i, &Font16, WHITE, BLACK);
-    } 
-    if (minusButton.contains(event.Xpoint, event.Ypoint)) {
-      Serial.println("minusButton");
-      i = i - 100;
-      if (i < 0)
-        i = i + 100;
-      digitButton.clear();
-      digitButton.displayNumber(i);
-      // GUI_DisNum(120, 70, i, &Font16, WHITE, BLACK);
-    } 
-    if (plusButton2.contains(event.Xpoint, event.Ypoint)) {
-      Serial.println("plusButton2");
-      i = i + 10;
-      if (i > 999)
-        i = i - 10;
-      digitButton.clear();
-      digitButton.displayNumber(i);
-      // GUI_DisNum(120, 70, i, &Font16, WHITE, BLACK);
-    } 
-    if (minusButton2.contains(event.Xpoint, event.Ypoint)) {
-      Serial.println("minusButton2");
-      i = i - 10;
-      if (i > 999)
-        i = i + 10;
-      digitButton.clear();
-      digitButton.displayNumber(i);
-      // GUI_DisNum(120, 70, i, &Font16, WHITE, BLACK);
-    } 
-    if (plusButton3.contains(event.Xpoint, event.Ypoint)) {
-      Serial.println("plusButton3");
-      i = i + 1;
-      if (i > 999)
-        i = i - 1;
-      digitButton.clear();
-      digitButton.displayNumber(i);
-      // GUI_DisNum(120, 70, i, &Font16, WHITE, BLACK);
-    } 
-    if (minusButton3.contains(event.Xpoint, event.Ypoint)) {
-      Serial.println("minusButton3");
-      i = i - 1;
-      if (i < 0)
-        i = i + 1;
-      digitButton.clear();
-      digitButton.displayNumber(i);
-      // GUI_DisNum(120, 70, i, &Font16, WHITE, BLACK);
-    } 
-    if (memoryButton.contains(event.Xpoint, event.Ypoint)) {
-      Serial.println("memoryButton");
-      EEPROM.write(0, i);
-      valueButton.clear();
-      valueButton.displayNumber(i);
-      delay(5000);
-    }
-    if (valueButton.contains(event.Xpoint, event.Ypoint)) {
-      Serial.println("valueButton");
-      i = EEPROM.read(0);
-      digitButton.clear();
-      digitButton.displayNumber(i);
-    }
-
-    if (memoryButton2.contains(event.Xpoint, event.Ypoint)) {
-      Serial.println("memoryButton2");
-      EEPROM.write(4, i);
-      valueButton2.clear();
-      valueButton2.displayNumber(i);
-      delay(5000);
-    }
-    if (valueButton2.contains(event.Xpoint, event.Ypoint)) {
-      Serial.println("valueButton2");
-      i = EEPROM.read(4);
-      digitButton.clear();
-      digitButton.displayNumber(i);
-    }
-
-    if (memoryButton3.contains(event.Xpoint, event.Ypoint)) {
-      Serial.println("memoryButton3");
-      EEPROM.write(8, i);
-      valueButton3.clear();
-      valueButton3.displayNumber(i);
-      delay(5000);
-    }
-    if (valueButton3.contains(event.Xpoint, event.Ypoint)) {
-      Serial.println("valueButton3");
-      i = EEPROM.read(8);
-      digitButton.clear();
-      digitButton.displayNumber(i);
-    }
-
-    if (memoryButton4.contains(event.Xpoint, event.Ypoint)) {
-      Serial.println("memoryButton4");
-      EEPROM.write(12, i);
-      valueButton4.clear();
-      valueButton4.displayNumber(i);
-      delay(5000);
-    }
-    if (valueButton4.contains(event.Xpoint, event.Ypoint)) {
-      Serial.println("valueButton4");
-      i = EEPROM.read(12);
-      digitButton.clear();
-      digitButton.displayNumber(i);
-    }
-
-    if (memoryButton5.contains(event.Xpoint, event.Ypoint)) {
-      Serial.println("memoryButton5");
-      EEPROM.write(16, i);
-      valueButton5.clear();
-      valueButton5.displayNumber(i);
-      delay(5000);
-    }
-    if (valueButton5.contains(event.Xpoint, event.Ypoint)) {
-      Serial.println("valueButton5");
-      i = EEPROM.read(16);
-      digitButton.clear();
-      digitButton.displayNumber(i);
+    for (int i = 0; i < sizeof(btn) / sizeof(btn[0]); i++) {
+      if (btn[i].contains(event.Xpoint, event.Ypoint)) {
+        switch (btn[i].class_id) {
+          case BUTTON_CLASS::MainButton:
+            Serial.println(btn[i].text);
+            break;
+          case BUTTON_CLASS::SpeedButton:
+            Serial.println(btn[i].text);
+            break;
+          case BUTTON_CLASS::DisplayButton:
+            handleDigit(i);
+            break;
+          case BUTTON_CLASS::MemoryButton:
+            performMemoryButtonAction(i);
+            break;
+          case BUTTON_CLASS::ValueButton:
+            updateValueButton(i);
+            break;
+        }
+        break;
+      }
     }
   }
   delay(10);
+}
 
+void handleDigit(int i)
+{
+  if (btn[i].id == DisplayButtonName::Plus1)
+  {
+    Serial.println("plusButton");
+    modifyDigit(i, 100);
+  }
+  else if (btn[i].id == DisplayButtonName::Minus1)
+  {
+    Serial.println("minusButton");
+    modifyDigit(i, -100);
+  }
+  else if (btn[i].id == DisplayButtonName::Plus2)
+  {
+    Serial.println("plusButton2");
+    modifyDigit(i, 10);
+  }
+  else if (btn[i].id == DisplayButtonName::Minus2)
+  {
+    Serial.println("minusButton2");
+    modifyDigit(i, -10);
+  }
+  else if (btn[i].id == DisplayButtonName::Plus3)
+  {
+    Serial.println("plusButton3");
+    modifyDigit(i, 1);
+  }
+  else if (btn[i].id == DisplayButtonName::Minus3)
+  {
+    Serial.println("minusButton3");
+    modifyDigit(i, -1);
+  }
+}
+void modifyDigit(int i, int val)
+{
+  num = num + val;
+  if (num > 999)
+    num = num - val;
+  btn[i].clear();
+  btn[i].displayNumber(num);
+}
+
+void performMemoryButtonAction(int i) {
+  Serial.println(btn[i].text);
+  EEPROM.write((btn[i].id - 1) * 2, num);
+  btn[i + 1].clear();
+  btn[i + 1].displayNumber(i);
+  delay(5000);
+}
+void updateValueButton(int index)
+{
+  Serial.println(btn[index].text);
+  Serial.println("valueButton5");
+  num = EEPROM.read((btn[index].id - 1) * 2);
+  btn[14].clear();
+  btn[14].displayNumber(num);
 }
 /*********************************************************************************************************
   END FILE
